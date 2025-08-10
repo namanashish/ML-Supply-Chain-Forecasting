501544938bec0955515845480b5c600c60ef7197
Naman Ashish <135628193+namanashish@users.noreply.github.com>
2025-08-10 21:50:10 +0530

    Update analysis.py

--- notebooks/analysis.py
+++ notebooks/analysis.py
@@ -1,12 +1,17 @@
 import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
+pred = pd.read_csv(r"C:\Users\naman\Downloads\ML-Driven Supply Chain Forecasting & Optimization\predictions\final_predictions.csv")
+submission=pd.read_csv(r"C:\Users\naman\Downloads\ML-Driven Supply Chain Forecasting & Optimization\data\raw\submission.csv")
 
+submission["PredictedSales"]=pred["PredictedSales"]
+submission.to_csv(r"C:\Users\naman\Downloads\ML-Driven Supply Chain Forecasting & Optimization\output\final_out.csv")
 
-df = pd.read_csv(r"C:\Users\naman\Downloads\ML-Driven Supply Chain Forecasting & Optimization\data\predictions\final_predictions.csv")
-
-
-print(df.head())
-print(df.describe())
-
-
-print("Nulls:", df['PredictedSales'].isnull().sum())
-print("Zeros:", (df['PredictedSales'] == 0).sum())
+rmse=np.sqrt(mean_absolute_error(submission["PredictedSales"],submission["Sales"]))
+mae=mean_absolute_error(submission["PredictedSales"],submission["Sales"])
+mse=mean_squared_error(submission["PredictedSales"],submission["Sales"])
+r2=r2_score(submission["PredictedSales"],submission["Sales"])
+print(rmse)
+print(mae)
+print(mse)
+print(r2)
